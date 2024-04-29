5aa05a238371a079cc2c3c1aa9915d714f35aec0
amitjan20 <aslekaramit@gmail.com>
2024-04-28 20:39:54 -0700

    Update ValidPalindrome.java

--- org.amit.pract/org/amit/programs/codinginterview/ValidPalindrome.java
+++ org.amit.pract/org/amit/programs/codinginterview/ValidPalindrome.java
@@ -1,1 +1,45 @@
+package com.ebay.sese.rest.helper;
+
+public class ValidPalindrome {
+
+    public static void main(String[] args) {
+        String palin = "abba";
+
+        System.out.println(isPalindrome(palin));
+    }
+
+    private static boolean isPalindrome(String palin) {
+
+        int i = 0;
+        int j = palin.length() - 1;
+
+        while (i < j) {
+
+            if(palin.charAt(i)!=palin.charAt(j)){
+                if(check(palin, i+1, j) || check(palin, i, j-1)){
+                    return true;
+                }
+            }
+
+            if (palin.charAt(i) != palin.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+
+    public static boolean check(String palin, int i, int j){
+        while(i<j){
+            if(palin.charAt(i)!=palin.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
 
