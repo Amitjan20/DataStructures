fb66e77d9db91a3f694c18a30ec095d8c2f45267
amitjan20 <aslekaramit@gmail.com>
2024-09-06 13:02:50 -0700

    Create Stock.java

---
+++ org.amit.pract/org/amit/programs/codinginterview/Stock.java
@@ -0,0 +1,22 @@
+public class Stock {
+
+    public static void main(String[] args) {
+        int[] arr = {7, 1, 5, 3, 6, 4};
+        System.out.println(maxProfit(arr));
+    }
+    
+    public static int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int minPrice = Integer.MAX_VALUE;
+        
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+        
+        return maxProfit;
+    }
+}
